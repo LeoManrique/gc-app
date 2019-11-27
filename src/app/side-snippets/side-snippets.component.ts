2327c931657343e324ab78a64dcf85e6cae8ef07
Leonardo <leonardomanrique43@gmail.com>
2019-11-27 16:40:37 -0500

    filtros

--- src/app/side-snippets/side-snippets.component.ts
+++ src/app/side-snippets/side-snippets.component.ts
@@ -10,6 +10,18 @@ export class SideSnippetsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    document.getElementById( 'snippet-sql' ).style.display = 'none';
+    document.getElementById( 'documento-java' ).style.display = 'none';
+  }
+
+  mostrarJava() {
+    document.getElementById( 'snippet-sql' ).style.display = 'none';
+    document.getElementById( 'documento-java' ).style.display = 'block';
+  }
+
+  mostrarSql() {
+    document.getElementById( 'snippet-sql' ).style.display = 'block';
+    document.getElementById( 'documento-java' ).style.display = 'none';
   }
 
 }
